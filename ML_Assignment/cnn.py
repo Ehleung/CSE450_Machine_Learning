6cc3e2e5b24bde5293bd3f0f33e79be3f1911d20
Ellery Leung <Ellery.h.leung@gmail.com>
2017-12-04 20:07:14 -0700

    added comments

--- ML_Assignment/cnn.py
+++ ML_Assignment/cnn.py
@@ -6,7 +6,11 @@ from keras import backend as K
 from keras.preprocessing import image
 import numpy as np
 
+# ELLERY LEUNG
+# 1207157168
+
 # SET THE DIMENSIONS OF THE IMAGES BEING USED
+# Given image dimensions of 150x150
 img_width, img_height = 150, 150
 
 # SET THE TRAIN, TEST AND VALIDATION DIRECTORIES
@@ -15,14 +19,16 @@ test_data_dir = './data/test'
 validation_data_dir = './data/validation'
 
 # SET THE NUMBER OF SAMPLES FOR TRAINING AND VALIDATION
+# Given values of 1000 training, 100 validation
 nb_train_samples = 1000
 nb_validation_samples = 100
 
 # SET THE NUMBER OF EPOCHS
-# 5, 10, 15, 20, 25
+# 5, 10, 15, 20, 25 -> recorded the current loss/accuracy at intervals of 5 in report
 epochs = 25
 
 # SET THE BATCH SIZE
+# Not specified, so I used batches of 100
 batch_size = 100
 
 if K.image_data_format() == 'channels_first':
@@ -33,28 +39,27 @@ else:
 # ADD CONVOLUTIONAL NEURAL NETWORK MODEL HERE
 # YOUR CODE GOES HERE
 
-#input_img = Input(shape=input_shape)
-
 #Define hyperparameters
 model = Sequential()
+# First layer of 32, activated by ReLU function, input shape is given to us
+# Kernel size is 3x3
 model.add(Conv2D(32, (3, 3), activation='relu', input_shape=input_shape))
+# Pool with a filter of 2x2
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+# Repeat with 64
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dense(512, activation='relu'))
+
+# Flatten for output of 1
 model.add(Flatten())
 model.add(Dropout(0.5))
 model.add(Dense(1, activation='softmax'))
 
-#model.add(Flatten())
-#model.add(Dense(512, activation='relu'))
-#model.add(Dropout(0.5))
-#model.add(Dense(1, activation='softmax'))
-
+# Compile model so it can be used, use binary_crossentropy because this is a binary classification.
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
 
 # THE FOLLOWING CODE WILL LOAD THE TRAINING AND VALIDATION DATA TO YOUR MODEL NAMED model
 train_datagen = ImageDataGenerator(
